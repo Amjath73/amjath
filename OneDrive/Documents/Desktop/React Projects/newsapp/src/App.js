d2c7fd754eb88e65077ba870a7ec51359d29275c
Amjath Sankar <amjathsanker4@gmail.com>
2024-06-08 16:25:32 +0530

    Important Completed

---
+++ OneDrive/Documents/Desktop/React Projects/newsapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ViewAll from './components/ViewAll';
+
+function App() {
+  return (
+    <div>
+      <ViewAll/>
+    </div>
+  );
+}
+
+export default App;
